9d15be2ffd3f2b2f1c62e833c2ca3e065c16e97d
Henrique A Cardoso <henrq.crdso@gmail.com>
2026-07-26 21:43:59 -0300

    Add files via upload

---
+++ include/decrypton/app.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+int run_capture(int argc, char** argv);
+int run_inspect(int argc, char** argv);
+int run_verify(int argc, char** argv);
+int run_diff(int argc, char** argv);
+int run_transform(int argc, char** argv);
